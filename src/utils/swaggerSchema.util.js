f2040e391c63d69f18abfa21323c435d5d5eb78b
Joo <vijak13019@hotmail.com>
2022-12-26 16:38:15 +0700

    feat: add swagger schema

---
+++ src/utils/swaggerSchema.util.js
@@ -0,0 +1,19 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Todo:
+ *       type: object
+ *       required: [todoText]
+ *       properties:
+ *         todoText:
+ *           type: string
+ *           description: Text of todo
+ *         _id:
+ *           type: string
+ *           description: Id of todo
+ *       example:
+ *         _id: randomId123
+ *         todoText: hello world
+ *
+ */
