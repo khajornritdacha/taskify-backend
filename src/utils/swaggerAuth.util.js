e6bced6cb4242b3700797356bbbaeb1d0270df1a
Joo <vijak13019@hotmail.com>
2022-12-26 16:38:37 +0700

    feat: add swagger for auth

---
+++ src/utils/swaggerAuth.util.js
@@ -0,0 +1,90 @@
+/**
+ * @swagger
+ *  tags:
+ *    name: Auth
+ *    description: Authentication
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login User
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *                 default: your secret password
+ *     responses:
+ *       200:
+ *         description: Login success
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *                 default: your secret password
+ *     responses:
+ *       201:
+ *         description: Register success
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     summary: Logout user
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       204:
+ *         description: Logout success
+ */
+
+/**
+ * @swagger
+ * /auth/token:
+ *   post:
+ *     summary: Generate access token by passing refresh token in header
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Generate Access Token successfully
+ */
