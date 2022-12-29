2a18ab42454052ef2e97198b4b7890ca18eb79e0
Joo <vijak13019@hotmail.com>
2022-12-29 15:20:45 +0700

    ci(app): add cors allow origins

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ const corsOptions = {
         'http://localhost:3000',
         'http://127.0.0.1:5173',
         'https://taskify-three.vercel.app',
+        'https://taskify-frontend.netlify.app/',
     ],
     credentials: true,
 };
